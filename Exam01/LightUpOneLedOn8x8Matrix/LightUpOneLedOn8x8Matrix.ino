8604c96c52cf472099dac80be4257b17192eb17f
Idar V <IdarV@users.noreply.github.com>
2015-09-26 19:36:11 +0200

    added description

--- Exam01/LightUpOneLedOn8x8Matrix/LightUpOneLedOn8x8Matrix.ino
+++ Exam01/LightUpOneLedOn8x8Matrix/LightUpOneLedOn8x8Matrix.ino
@@ -1,3 +1,10 @@
+/*
+* This script will light up one led on a 8x8 matrix.
+* Both cables should be added with a 220 ohm resistor.
+* Connect pin 8 to a col, and pin 7 to a row.
+* Follow the matrix-pin-sheet in this project.
+*/
+
 const int col = 8;
 const int row = 7;
 void setup() {

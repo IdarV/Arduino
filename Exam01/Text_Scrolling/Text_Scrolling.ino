d0f77876fdad3a46134fa15f8102186a07049e82
IdarV <tjoida13@nith.no>
2015-09-28 19:54:13 +0200

    some updates, some sketches

--- Exam01/Text_Scrolling/Text_Scrolling.ino
+++ Exam01/Text_Scrolling/Text_Scrolling.ino
@@ -8,17 +8,6 @@ LedControl lc = LedControl(12, 11, 10, 1);
 // LOADCS = 10
 // NUMBER_OF_IC = 1
 
-const int delay_time = 50;
-
-void setup() {
-  // the zero refers to the MAX7219 number, it is zero for 1 chip
-  lc.shutdown(0, false); // turn off power saving, enables display
-  lc.setIntensity(0, 15); // sets brightness (0~15 possible values)
-  lc.clearDisplay(0);// clear screen
-}
-
-
-
 int empty = B0000000;
 
 int a[8] = {
@@ -54,17 +43,40 @@ int space[] = {
   B00000000
 };
 
-void scroll(int letter[]){
-  int i, j, trip_index;
-  int trip = 0;
+const int delay_time = 50;
+const int TEXT_SIZE = 100;
+int index = 0;
 
+int text[TEXT_SIZE][8];
+// dynamic allocation
+int** ary = new int*[8];
+
+
+void setup() {
+  // the zero refers to the MAX7219 number, it is zero for 1 chip
+  lc.shutdown(0, false); // turn off power saving, enables display
+  lc.setIntensity(0, 15); // sets brightness (0~15 possible values)
+  lc.clearDisplay(0);// clear screen
+  // init array
+  for(int i = 0; i < TEXT_SIZE; ++i)
+      ary[i] = new int[8];
+}
+
+
+
+
+
+void scroll(){
+  int i, j, i_i, trip_index;
+  int trip = 0;
+  for(i_i = 0; i_i < index; i_i++){
   for (i = 0; i <= 7; i++) {
     trip++;
     trip_index = 0;
 
     for (j = 0; j <= 7; j++) {
       if (j + trip > 7) {
-        lc.setRow(0, j, letter[trip_index++]);
+        lc.setRow(0, j, ary[i_i][trip_index++]);
       } else {
         lc.setRow(0, j, empty);
       }
@@ -78,24 +90,32 @@ void scroll(int letter[]){
 
     for (j = 7; j >= 0; j--) {
       if (j + trip <= 7) {
-        lc.setRow(0, j, letter[trip_index--]);
+        lc.setRow(0, j, ary[i_i][trip_index--]);
       } else {
         lc.setRow(0, j, empty);
       }
 
     } delay(delay_time);
   }
+  trip = 0;
+  }
 
   lc.clearDisplay(0);
 }
-int text[100][8];
 
+void add(int nee[] ){
+  ary[index++] =nee;
+}
 
 void loop() {
-  scroll(a);
-  scroll(y);
-  scroll(y);
-  scroll(space);
+  add(a);
+  add(y);
+  add(y);
+  add(space);
+//  scroll(a);
+//  scroll(y);
+//  scroll(y);
+  scroll();
   lc.clearDisplay(0);
 }
 

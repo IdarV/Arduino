4493efeae6d2cb6bc8f078eef09631388bee5c61
IdarV <tjoida13@nith.no>
2015-10-28 19:53:38 +0100

    basic alarm now works

--- Exam02/AlarmClock/AlarmClock.ino
+++ Exam02/AlarmClock/AlarmClock.ino
@@ -7,13 +7,168 @@
 #define dc   9
 #define rst  8
 
-TFT TFTscreen = TFT(cs, dc, rst);
+// Init screen
+TFT tft = TFT(cs, dc, rst);
 
+// Declare Real Time Clock
 RTC_DS1307 rtc;
 
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-char monthsOfTheYear[12][12] = {"January", "February", "March", "April", "May", "June", "July",
-                             "August", "September", "October", "November", "December"};
+char daysOfTheWeek[7][12] =
+{"Sunday", "Monday", "Tuesday", "Wednesday",
+ "Thursday", "Friday", "Saturday"};
+char monthsOfTheYear[12][12] =
+{"January", "February", "March", "April", "May", "June", "July",
+"August", "September", "October", "November", "December"};
+
+// SCREEN SETTINGS
+int defaultBackground = 0x0000;
+
+// ALARM SETTINGS
+DateTime alarmMillis;
+int alarmTonePin = 2;
+int alarmTone;
+int alarmButtonPin = 4;
+int alarmButtonPressed = 0;
+
+// YEAR SETTINGS
+int yearSize = 2;
+int yearXPos = 114;
+int yearYPos = 110;
+int yearLength = 48;
+
+// MONTH SETTINGS
+int monthSize = 2;
+int monthXPos = 0;
+int monthYPos = 0;
+int monthLength = 86;
+
+// DAY OF MONTH SETTINGS
+int daySize = 2;
+int dayXPos = monthLength + 4; // One space after month
+int dayYPos = 0;
+int dayLength = 24;
+
+// Weekday SETTINGS
+int weekdayColor = 0xFF00FF;
+int weekdaySize = 2;
+int weekdayXPos = 0; // One space after month
+int weekdayYPos = 110;
+int weekdayLength = 24;
+
+// HOUR SETTINGS
+int hourLeftOffset = 11;
+int hourSize = 4;
+int hourXPos = 0 + hourLeftOffset;
+int hourYPos = 50;
+int hourLength = 48;
+
+// Minute SETTINGS
+int minuteColor = 0xFF00FF;
+int minuteSize = 4;
+int minuteXPos = hourLength + hourLeftOffset;
+int minuteYPos = 50;
+int minuteLength = 48;
+
+// Second SETTINGS
+int secondSize = 4;
+int secondXPos = minuteXPos + minuteLength;
+int secondYPos = 50;
+int secondLength = 48;
+
+void clearTextLine(int textX, int textY, int textSize, int textLength) {
+  for (int i = 0; i < (8 * textSize); i++) {
+    tft.drawFastHLine(textX, textY + i, textLength, defaultBackground);
+  }
+}
+
+void writeMonth(DateTime time_now) {
+  tft.stroke(255, 255, 255);
+  tft.setTextSize(monthSize);
+  tft.setCursor(monthXPos, monthYPos);
+
+  tft.print(monthsOfTheYear[time_now.month() - 1]);
+}
+
+void writeYear(DateTime time_now) {
+  tft.stroke(255, 255, 255);
+  tft.setTextSize(yearSize);
+  tft.setCursor(yearYPos, yearXPos);
+
+  tft.print(time_now.year());
+}
+
+void writeDay(DateTime time_now) {
+  tft.stroke(255, 255, 255);
+  tft.setTextSize(daySize);
+  tft.setCursor(dayXPos, dayYPos);
+
+  if (time_now.day() < 10) {
+    tft.print("0");
+  }
+
+  tft.print(time_now.day());
+}
+
+void writeWeekDay(DateTime time_now){
+  tft.stroke(weekdayColor);
+  tft.setTextSize(weekdaySize);
+  tft.setCursor(weekdayXPos, weekdayYPos);
+
+  tft.print(daysOfTheWeek[time_now.dayOfTheWeek()]);
+}
+
+void writeHour(DateTime time_now) {
+  tft.stroke(255, 255, 255);
+  tft.setTextSize(hourSize);
+  tft.setCursor(hourXPos, hourYPos);
+
+  tft.print(time_now.hour());
+}
+
+void writeMinute(DateTime time_now) {
+  tft.stroke(minuteColor);
+  tft.setTextSize(hourSize);
+  tft.setCursor(minuteXPos, minuteYPos);
+  if (time_now.minute() < 10) {
+    tft.print("0");
+  }
+  tft.print(time_now.minute());
+}
+
+void writeSecond(DateTime time_now) {
+  tft.stroke(255, 255, 255);
+  tft.setTextSize(secondSize);
+  tft.setCursor(secondXPos, secondYPos);
+  if (time_now.second() < 10) {
+    tft.print("0");
+  }
+  tft.print(time_now.second());
+}
+
+DateTime getTime(){
+  return rtc.now() + TimeSpan(0, 0, 21, 16);//timeDifference;
+}
+
+void initScreen(){
+  tft.background(0, 0, 0);
+  DateTime time_now = getTime();
+
+  writeSecond(time_now);
+  writeMinute(time_now);
+  writeHour(time_now);
+  writeDay(time_now);
+  writeWeekDay(time_now);
+  writeMonth(time_now);
+  writeYear(time_now);
+}
+
+void initAlarm(){
+  alarmTone = 1480;
+  alarmMillis = DateTime(getTime() + TimeSpan(0, 0, 0, 10));
+
+  pinMode(alarmTonePin, OUTPUT);
+  pinMode(alarmButtonPin, OUTPUT);
+}
 
 void setup() {
   Serial.begin(9600);
@@ -29,46 +184,72 @@ void setup() {
     Serial.println("RTC time set to compile-date");
   }
 
-  TFTscreen.begin();
-  TFTscreen.background(0, 0, 0);
+  tft.begin();
+
+  initScreen();
+  initAlarm();
+
 }
 
-void clear_text_line(int text_x, int text_y){
-  for(int i = 0; i < 16; i++){
-    TFTscreen.drawFastHLine(text_x, text_y + i, 100,0x0000);  
+bool alarmButtonIsPressed(){
+  return HIGH == digitalRead(alarmButtonPin);
+}
+
+void waitForNextSecond(DateTime time_now){
+  // Wait for new second (I like this better than delay(1000), because it takes loop execution time into account)
+  while(getTime().second() == time_now.second()){
+    if(alarmMillis.unixtime() < time_now.unixtime() && alarmButtonPressed == 0){
+      if(alarmButtonIsPressed()){
+        alarmButtonPressed = 1;
+      }
+      tone(2, alarmTone, 10);
+    }
+    delay(10);
+  }
+}
+
+void updateDisplayTime(DateTime time_now){
+  clearTextLine(secondXPos, secondYPos, secondSize, secondLength);
+  writeSecond(time_now);
+
+  // If second is 0, change the minute aswell
+  if (time_now.second() == 0) {
+    clearTextLine(minuteXPos, minuteYPos, minuteSize, minuteLength);
+    writeMinute(time_now);
+
+    // If the second is 0 AND minute is 0, bump the hour up
+    if (time_now.minute() == 0) {
+      clearTextLine(hourXPos, hourYPos, hourSize, hourLength);
+      writeHour(time_now);
+
+      // If the second is 0 AND the minute is 0 AND the hour is 0, skip to next day
+      if (time_now.hour() == 0) {
+        clearTextLine(dayXPos, dayYPos, daySize, dayLength);
+        clearTextLine(weekdayXPos, weekdayYPos, weekdaySize, weekdayLength);
+        writeDay(time_now);
+        writeWeekDay(time_now);
+
+        // If the second is 0 AND the minute is 0 AND the hour is 0 AND the day is 1, skip to next day (preferebly friday at 6pm)
+        if (time_now.day() == 1) {
+          clearTextLine(monthXPos, monthYPos, monthSize, monthLength);
+          writeMonth(time_now);
+
+          // If the second is 0 AND the minute is 0 AND the hour is 0 AND the day is 1 AND the month is 1, skip to next year and RIDE YOUR HOVERBOARDS
+          if (time_now.month() == 1) {
+            clearTextLine(yearXPos, yearYPos, yearSize, yearLength);
+            writeYear(time_now);
+          }
+        }
+      }
+    }
   }
 }
 
 void loop() {
-  DateTime now = rtc.now();
-  String timeNow = "";
-  timeNow += now.year();
-  timeNow += "\n";
-  timeNow += monthsOfTheYear[now.month()];
-  timeNow += "\n";
-  timeNow += (daysOfTheWeek[now.dayOfTheWeek()]);
-  timeNow += "\n";
-  int now_hour = now.hour();
-  if (now_hour < 10) {
-    timeNow += "0";
-  }
-  timeNow += now_hour;
-  timeNow += ":";
-  timeNow += now.minute();
-  timeNow += ":";
-  int now_second = now.second();
-  if (now_second < 10) {
-    timeNow += "0";
-  }
-  timeNow += now_second;
-  Serial.println(timeNow);
-
-  TFTscreen.stroke(255, 255, 255);
-  TFTscreen.setTextSize(2);
-  TFTscreen.setCursor(0, 0);
-  TFTscreen.println(timeNow);
-  
-  delay(1000);
-  clear_text_line(0, 46);
-
+  // Get time
+  DateTime time_now = getTime();
+  // Update display
+  updateDisplayTime(time_now);
+  // Wait for next second from clock;
+  waitForNextSecond(time_now);
 }
